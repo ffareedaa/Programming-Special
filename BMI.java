2ac04c9b89b8596f6ecb7dcb1cc8d4a220e5e914
ffareedaa <43241821+ffareedaa@users.noreply.github.com>
2018-09-13 20:54:32 +0700

    60050226 ฟารีดา วานิชยากร

---
+++ BMI.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class BMI {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double weight;
+        double height;
+        double bmi;
+
+        System.out.print("Enter your weight (kilogram) => ");
+        weight = sc.nextDouble();
+
+        System.out.print("Enter your height (meter) => ");
+        height = sc.nextDouble();
+
+        bmi = weight/(height*height);
+        System.out.println("Your BMI is "+bmi);
+		System.out.print("Status ");
+
+        if (bmi < 18.5 ) 
+			{System.out.println("Underweight : Under 18.5");}
+        else if (bmi >= 18.5 && bmi < 25) 
+			{System.out.println("Normal : 18.5-24.9");}
+        else if (bmi >= 25 && bmi < 30) 
+			{System.out.println("Overweight : 25-29.9");}
+        else if (bmi >= 30) 
+			{System.out.println("Obese : 30 or over");}
+     }
+    }
